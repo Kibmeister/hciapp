6d8d821a60802dbbbe2fff86f2f476f90dd5d9a5
Frederik Kjaer Andersen <201806216@post.au.dk>
2019-10-09 10:57:31 +0200

    Added settings button

--- app/src/main/java/hci/app/ProfileFragment.java
+++ app/src/main/java/hci/app/ProfileFragment.java
@@ -3,11 +3,16 @@ package hci.app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,12 +25,33 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+    NavController navController = new NavController(null);
+    Button settingsButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        return v;
     }
 
+    /* public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        settingsButton = view.findViewById(R.id.settingsButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_profileFragment_to_settingsFragment);
+            }
+        });
+    }
+    */
 }
