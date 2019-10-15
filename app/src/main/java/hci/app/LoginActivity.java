0c67d9142a9ac424e3a55ff670397a6217fe55b9
Frederik Kjaer Andersen <201806216@post.au.dk>
2019-10-15 12:01:26 +0200

    facebook login now works

---
+++ app/src/main/java/hci/app/LoginActivity.java
@@ -0,0 +1,71 @@
+package hci.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.fblogin_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile","email"));
+        callbackManager = CallbackManager.Factory.create();
+
+        Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("Login success");
+                Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(MainIntent);
+            }
+
+            @Override
+            public void onCancel() {
+                System.out.println("Login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                System.out.println("Login failed");
+            }
+        });
+
+        // Check if a user is already signed in
+        try {
+            Thread.sleep(1000);
+            AccessToken accessToken = AccessToken.getCurrentAccessToken();
+            boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+            if (isLoggedIn) {
+                startActivity(MainIntent);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
