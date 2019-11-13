6921508a5d75d553b6558871d94f4d29b67b0753
Frederik Kjaer Andersen <201806216@post.au.dk>
2019-11-13 17:59:45 +0100

    Map now only allows one custom marker at any time

--- app/src/main/java/hci/app/MapFragment.java
+++ app/src/main/java/hci/app/MapFragment.java
@@ -3,30 +3,37 @@ package hci.app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
 
     public MapFragment() {
         // Required empty public constructor
     }
 
+    private SupportMapFragment map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +41,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
-        SupportMapFragment map = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        map = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         map.getMapAsync(this);
 
         return v;
@@ -43,10 +50,37 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap mMap) {
         System.out.println("Map ready, placing marker");
-        
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        setMapLongClick(mMap);
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    // Save temporary marker as variable
+    private Marker marker;
+
+    public void setMapLongClick(final GoogleMap map) {
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+
+            public void onMapLongClick(LatLng latLng) {
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                marker = map.addMarker(new MarkerOptions().position(latLng)
+                        .title("Create new event here?"));
+            }
+        });
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        Toast.makeText(getContext(), "Marker clicked", Toast.LENGTH_LONG).show();
+        return false;
     }
 }
