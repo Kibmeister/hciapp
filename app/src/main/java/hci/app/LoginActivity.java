d2480eb431cdad9c85f89ecdab6ce5854ddd4af3
Frederik Kjaer Andersen <201806216@post.au.dk>
2019-11-13 17:50:50 +0100

    Removed KeyHash gen from LoginActivity class

--- app/src/main/java/hci/app/LoginActivity.java
+++ app/src/main/java/hci/app/LoginActivity.java
@@ -1,10 +1,7 @@
 package hci.app;
 
 import android.content.Intent;
-import android.content.pm.*;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +13,6 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public class LoginActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
@@ -27,22 +21,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
-
         setContentView(R.layout.activity_login);
 
         loginButton = findViewById(R.id.fblogin_button);
@@ -67,8 +45,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onError(FacebookException error) {
                 Toast.makeText(getApplicationContext(), "An error occured while logging in", Toast.LENGTH_LONG).show();
-                error.printStackTrace();
-
             }
         });
 
